9d4cb649c4a5cdadd5306a737f89cf34655354d7
Helen Risky Dwi Wahyuni <riskyhelen1@gmail.com>
2026-04-30 01:03:43 +0700

    feat: register user working with Room database

--- app/src/main/java/com/upn/catatlari/view/RegisterFragment.kt
+++ app/src/main/java/com/upn/catatlari/view/RegisterFragment.kt
@@ -4,18 +4,56 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.upn.catatlari.data.local.database.AppDatabase
+import com.upn.catatlari.data.local.entity.UserEntity
 import com.upn.catatlari.databinding.FragmentRegisterBinding
+import kotlinx.coroutines.launch
 
 class RegisterFragment : Fragment() {
 
     private lateinit var binding: FragmentRegisterBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        // Inflate the layout for this fragment
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+
         binding = FragmentRegisterBinding.inflate(inflater, container, false)
+
+        binding.buttonRegister.setOnClickListener {
+
+            val nama = binding.etNameSignup.text.toString()
+            val email = binding.etEmailSignup.text.toString()
+            val password = binding.etPasswordSignup.text.toString()
+            val retypePassword = binding.etRetypePasswordSignup.text.toString()
+
+            if (nama.isEmpty() || email.isEmpty() || password.isEmpty() || retypePassword.isEmpty()) {
+                Toast.makeText(requireContext(), "Isi semua data!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (password != retypePassword) {
+                Toast.makeText(requireContext(), "Password tidak sama!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val db = AppDatabase.getDatabase(requireContext())
+            val userDao = db.userDao()
+
+            lifecycleScope.launch {
+                userDao.insertUser(
+                    UserEntity(
+                        username = email,
+                        password = password
+                    )
+                )
+
+                Toast.makeText(requireContext(), "Registrasi berhasil!", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         return binding.root
     }
-
 }
