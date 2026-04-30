473d8e9ea9db983f4a4101370bb67e067b344092
Helen Risky Dwi Wahyuni <riskyhelen1@gmail.com>
2026-04-30 23:11:45 +0700

    feat: implement RunViewModel with LiveData

--- app/src/main/java/com/upn/catatlari/viewmodel/RunViewModel.kt
+++ app/src/main/java/com/upn/catatlari/viewmodel/RunViewModel.kt
@@ -7,30 +7,18 @@ import com.upn.catatlari.model.Run
 
 class RunViewModel : ViewModel() {
 
-    val runList = listOf(
-        Run(runDate = "22 Mei 2026", runDistance = 1, runDuration = 3),
-        Run(runDate = "23 Mei 2026", runDistance = 1, runDuration = 3),
-        Run(runDate = "24 Mei 2026", runDistance = 1, runDuration = 3)
-    )
+    private val runListLiveData = MutableLiveData<List<Run>>(emptyList())
+    var runHistory: LiveData<List<Run>> = runListLiveData
 
-    private val runListLiveData = MutableLiveData(runList) // variabel yang berfungsi mengatur perubahan data
-    var runHistory : LiveData<List<Run>> = runListLiveData // variabel yang dipakai untuk dipanggil class lain
-
-    // CREATE
     fun addRun(run: Run) {
-//        if (runListLiveData.value == null) {
-//
-//        }
-
         val currentList = runListLiveData.value.orEmpty().toMutableList()
-        currentList.add(run)
+        currentList.add(0, run)
         runListLiveData.value = currentList
     }
+}
 
     // READ
 
     // UPDATE
 
     // DELETE
-
-}
